8760503784a0918382e935ded73b99ec789e7821
Julian Schönbächler <info@julian-s.ch>
2017-04-03 15:22:22 +0200

    Fully controllable sheep.

--- Assets/Adventure/2_Code/Character/SheepController.cs
+++ Assets/Adventure/2_Code/Character/SheepController.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 [RequireComponent(typeof(CapsuleCollider), typeof(Rigidbody))]
 public class SheepController : MonoBehaviour
 {
+	public enum CharacterTransform
+	{
+		xAxisUp,
+		yAxisUp,
+		zAxisUp
+	}
+
+	public CharacterTransform characterTransform;
 	[SerializeField] protected float _walkSpeed = 1f;
-	[SerializeField] protected float _runSpeed = 1f;
 	[SerializeField] protected float _turnSpeed = 1f;
 	[SerializeField] protected float _jumpForce = 1f;
-	[SerializeField] protected float _gravity = 1f;
 
 	private float _jumpCooldown = 0.1f;
 	private float _time = 0f;
+	private Vector3 _currentVelocity;
+	private Vector3 _velocityDir;
 
-	public bool IsGrounded { get; set; }
+	public bool IsGrounded { get; private set; }
 
 	protected Rigidbody _rigidbody;
 
@@ -29,38 +37,38 @@ public class SheepController : MonoBehaviour
 		float horizontalAxis = Input.GetAxis("Horizontal");
 		float verticalAxis = Input.GetAxis("Vertical");
 
-		transform.Rotate(Vector3.forward * horizontalAxis * _turnSpeed * Time.deltaTime * 100f);
+		_currentVelocity = _rigidbody.velocity;
 
-		/*
-		if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-			_rigidbody.velocity = new Vector3(_rigidbody.velocity.x,
-				-verticalAxis * _runSpeed * Time.deltaTime * 100f,
-				_rigidbody.velocity.z);
-		else
-			_rigidbody.velocity = new Vector3(_rigidbody.velocity.x,
-				-verticalAxis * _walkSpeed * Time.deltaTime * 100f,
-				_rigidbody.velocity.z);
-		*/
-		
-		if(Input.GetKeyDown(KeyCode.Space) && IsGrounded)
+		switch(characterTransform)
 		{
-			_rigidbody.velocity = new Vector3(_rigidbody.velocity.x, _jumpForce, _rigidbody.velocity.z);
-			_time = Time.time + _jumpCooldown;
-			IsGrounded = false;
-			//IsGrounded = false;
-			//_ignoreGrounded = 5;
+			case CharacterTransform.xAxisUp:
+				_velocityDir = -verticalAxis * _walkSpeed * transform.forward;
+				transform.Rotate(Vector3.right * horizontalAxis * _turnSpeed * Time.deltaTime * 100f);
+				break;
+
+			case CharacterTransform.yAxisUp:
+				_velocityDir = -verticalAxis * _walkSpeed * transform.forward;
+				transform.Rotate(Vector3.up * horizontalAxis * _turnSpeed * Time.deltaTime * 100f);
+				break;
+
+			default:
+				_velocityDir = -verticalAxis * _walkSpeed * transform.up;
+				transform.Rotate(Vector3.forward * horizontalAxis * _turnSpeed * Time.deltaTime * 100f);
+				break;
+				
 		}
 
-		Vector3 currentVelocity = _rigidbody.velocity;
-		currentVelocity.x = verticalAxis * _walkSpeed;
-		/*
-		// Collision fix: isGrounded detection
-		if(_ignoreGrounded > 0)
-			_ignoreGrounded--;
 
-		if(!IsGrounded)
-			_rigidbody.velocity -= Vector3.up * _gravity * Time.deltaTime * 100f;
-		*/
+		if(Input.GetKeyDown(KeyCode.Space) && IsGrounded)
+		{
+			_currentVelocity.y = _jumpForce;
+			_time = Time.time + _jumpCooldown;
+			IsGrounded = false;
+		}
+
+		_currentVelocity.x = _velocityDir.x;
+		_currentVelocity.z = _velocityDir.z;
+		_rigidbody.velocity = _currentVelocity;
 	}
 
 
@@ -84,10 +92,4 @@ public class SheepController : MonoBehaviour
 			}
 		}
 	}
-	/*
-	void OnCollisionExit(Collision collisionInfo)
-	{
-		IsGrounded = false;
-	}
-	*/
 }
