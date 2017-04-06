1d2badd0c2d4fcddd36c3c0f490513d61fa82348
Julian Schönbächler <info@julian-s.ch>
2017-04-06 13:01:29 +0200

    Working on Inventory...

--- Assets/Adventure/2_Code/Inventory/Inventory.cs
+++ Assets/Adventure/2_Code/Inventory/Inventory.cs
@@ -3,5 +3,11 @@ using System.Collections.Generic;
 
 public class Inventory
 {
-	
+	protected int _capacity;
+	protected Dictionary<string, Item> _inventory;
+
+	public Inventory(int capacity)
+	{
+		_capacity = capacity;
+	}
 }
---
+++ Assets/Adventure/2_Code/Inventory/Item.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+	public string ID { get; protected set; }
+	public string Name { get; protected set; }
+	public string Description { get; protected set; }
+	public Texture2D Icon { get; set; }
+}
