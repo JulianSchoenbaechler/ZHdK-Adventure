d5ad352b717c86637e8a5f953c020a326509c15d
Julian <info@julian-s.ch>
2017-05-15 01:20:58 +0200

    Created model and material prefabs. + Basic animation for barn door.

--- Assets/Adventure/2_Code/Character/FarmerShootController.cs
+++ Assets/Adventure/2_Code/Character/FarmerShootController.cs
@@ -5,68 +5,101 @@ using JulianSchoenbaechler.SnapPanel;
 using Adventure.CameraHandling;
 using Adventure.UI;
 
-[RequireComponent(typeof(CapsuleCollider), typeof(Rigidbody), typeof(Animator))]
-public class FarmerShootController : MonoBehaviour
+namespace Adventure.Character
 {
-	[SerializeField] protected bool _active = true;
-	[SerializeField] protected Transform _target;
-	[SerializeField] protected float _maxDistance = 10f;
-	[SerializeField] protected float _yOffset = 0f;
-	[SerializeField] protected float _pauseInterval = 5f;
-	[SerializeField] protected float _searchTime = 1f;
-	[SerializeField] protected float _loadTime = 1f;
-	[SerializeField] protected float _turnSpeed = 1f;
-	[SerializeField] protected Transform _bulletSpawnPosition;
-	[SerializeField] protected Animator _visualShotAnimator;
-	[SerializeField] protected Animator _visualHitAnimator;
-	[SerializeField] protected UISpray _shotNoise;
-
-	protected Animator _animator;
-	protected SnapPanel _snapPanel;
-
-	private float _timer = 0f;
-	private Quaternion _rotateToTarget;
-	private RaycastHit _shotHit;
-
-	// Use this for initialization
-	void Start()
+	[RequireComponent(typeof(CapsuleCollider), typeof(Rigidbody), typeof(Animator))]
+	public class FarmerShootController : MonoBehaviour
 	{
-		_animator = GetComponent<Animator>();
-		_snapPanel = GetComponentInChildren<SnapPanel>();
-		_snapPanel.Invoke("StartSnapping", _pauseInterval + 2f);
-	}
-	
-	// Update is called once per frame
-	void Update()
-	{
-		//Debug.DrawRay(_bulletSpawnPosition.position, _target.position - _bulletSpawnPosition.position + Vector3.up * _yOffset, Color.red);
+		[SerializeField] protected bool _active = true;
+		[SerializeField] protected Transform _target;
+		[SerializeField] protected float _maxDistance = 10f;
+		[SerializeField] protected float _yOffset = 0f;
+		[SerializeField] protected float _pauseInterval = 5f;
+		[SerializeField] protected float _searchTime = 1f;
+		[SerializeField] protected float _loadTime = 1f;
+		[SerializeField] protected float _turnSpeed = 1f;
+		[SerializeField] protected Transform _bulletSpawnPosition;
+		[SerializeField] protected Animator _visualShotAnimator;
+		[SerializeField] protected Animator _visualHitAnimator;
+		[SerializeField] protected UISpray _shotNoise;
 
-		if(!_active)
-			return;
+		protected Animator _animator;
+		protected SnapPanel _snapPanel;
 
-		if(Vector3.Distance(transform.position, _target.position) > _maxDistance)
-			return;
+		private float _timer = 0f;
+		private Quaternion _rotateToTarget;
+		private RaycastHit _shotHit;
 
-		// Rotate to target
-		_rotateToTarget = Quaternion.LookRotation(_target.position - transform.position, Vector3.up);
-		transform.rotation = Quaternion.Lerp(transform.rotation, _rotateToTarget, Time.deltaTime * _turnSpeed);
 
-		// Idle, Load, Shoot
-		if(_animator.GetBool("Searching"))
+		void Start()
 		{
-			// Searching...
+			_animator = GetComponent<Animator>();
+			_snapPanel = GetComponentInChildren<SnapPanel>();
+			_snapPanel.Invoke("StartSnapping", _pauseInterval + 2f);
+		}
+		
 
+		void Update()
+		{
+			//Debug.DrawRay(_bulletSpawnPosition.position, _target.position - _bulletSpawnPosition.position + Vector3.up * _yOffset, Color.red);
 
-			if(_animator.GetBool("Set"))
+			if(!_active)
+				return;
+
+			if(Vector3.Distance(transform.position, _target.position) > _maxDistance)
+				return;
+
+			// Rotate to target
+			_rotateToTarget = Quaternion.LookRotation(_target.position - transform.position, Vector3.up);
+			transform.rotation = Quaternion.Lerp(transform.rotation, _rotateToTarget, Time.deltaTime * _turnSpeed);
+
+			// Idle, Load, Shoot
+			if(_animator.GetBool("Searching"))
 			{
-				// Ready to shoot...
+				// Searching...
 
-				if(_timer >= _loadTime)
+
+				if(_animator.GetBool("Set"))
 				{
-					// Shoot!
-					_animator.SetTrigger("Shoot");
-					_animator.SetBool("Searching", false);
-					_animator.SetBool("Set", false);
+					// Ready to shoot...
+
+					if(_timer >= _loadTime)
+					{
+						// Shoot!
+						_animator.SetTrigger("Shoot");
+						_animator.SetBool("Searching", false);
+						_animator.SetBool("Set", false);
+						_timer = 0f;
+					}
+					else
+					{
+						_timer += Time.deltaTime;
+					}
+				}
+				else
+				{
+					// Ready to load...
+
+					if(_timer >= _searchTime)
+					{
+						// Load!
+						_animator.SetBool("Set", true);
+						_timer = 0f;
+					}
+					else
+					{
+						_timer += Time.deltaTime;
+					}
+				}
+			}
+			else
+			{
+				// Idle...
+
+				if(_timer >= _pauseInterval)
+				{
+					// Start searching!
+					_animator.SetBool("Searching", true);
 					_timer = 0f;
 				}
 				else
@@ -74,78 +107,48 @@ public class FarmerShootController : MonoBehaviour
 					_timer += Time.deltaTime;
 				}
 			}
-			else
-			{
-				// Ready to load...
+		}
 
-				if(_timer >= _searchTime)
+		public void OnReloadGun()
+		{
+			//GetComponentInChildren<SnapPanel>().StartSnapping();
+			print("reload");
+		}
+
+		public void OnShootGun()
+		{
+			_visualShotAnimator.SetTrigger("VisualShot");
+			_shotNoise.Play();
+
+			if(Physics.Raycast(
+				_bulletSpawnPosition.position,
+				_target.position - _bulletSpawnPosition.position + Vector3.up * _yOffset,
+				out _shotHit,
+				_maxDistance
+			))
+			{
+				_visualHitAnimator.transform.position = _shotHit.point;
+				_visualHitAnimator.transform.rotation = CameraControl.active.transform.rotation;
+				_visualHitAnimator.SetTrigger("VisualHit");
+
+				if(_shotHit.transform.CompareTag("Player"))
 				{
-					// Load!
-					_animator.SetBool("Set", true);
-					_timer = 0f;
+					print("Dead");
 				}
 				else
 				{
-					_timer += Time.deltaTime;
+					print("Missed");
 				}
 			}
+
+			_snapPanel.Invoke("StartSnapping", _pauseInterval + 1.3f);
 		}
-		else
+
+
+		public bool Active
 		{
-			// Idle...
-
-			if(_timer >= _pauseInterval)
-			{
-				// Start searching!
-				_animator.SetBool("Searching", true);
-				_timer = 0f;
-			}
-			else
-			{
-				_timer += Time.deltaTime;
-			}
+			get { return _active; }
+			set { _active = value; }
 		}
-	}
-
-	public void OnReloadGun()
-	{
-		//GetComponentInChildren<SnapPanel>().StartSnapping();
-		print("reload");
-	}
-
-	public void OnShootGun()
-	{
-		_visualShotAnimator.SetTrigger("VisualShot");
-		_shotNoise.Play();
-
-		if(Physics.Raycast(
-			_bulletSpawnPosition.position,
-			_target.position - _bulletSpawnPosition.position + Vector3.up * _yOffset,
-			out _shotHit,
-			_maxDistance
-		))
-		{
-			_visualHitAnimator.transform.position = _shotHit.point;
-			_visualHitAnimator.transform.rotation = CameraControl.active.transform.rotation;
-			_visualHitAnimator.SetTrigger("VisualHit");
-
-			if(_shotHit.transform.CompareTag("Player"))
-			{
-				print("Dead");
-			}
-			else
-			{
-				print("Missed");
-			}
-		}
-
-		_snapPanel.Invoke("StartSnapping", _pauseInterval + 1.3f);
-	}
-
-
-	public bool Active
-	{
-		get { return _active; }
-		set { _active = value; }
 	}
 }
---
+++ Assets/Adventure/2_Code/Entity/OpenBarnDoor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Adventure.Entity
+{
+	[RequireComponent(typeof(Collider), typeof(Animator))]
+	public class OpenBarnDoor : MonoBehaviour
+	{
+		protected Animator _animator;
+
+
+		protected virtual void Start()
+		{
+			_animator = GetComponent<Animator>();
+		}
+
+		protected virtual void OnCollisionEnter(Collision collision)
+		{
+			if(collision.transform.CompareTag("Barrel"))
+			{
+				_animator.SetTrigger("Open");
+			}
+		}
+	}
+}
