76c7a7e63829fee0af34c0c709a32ce93b1d2083
Julian Schönbächler <info@julian-s.ch>
2017-05-23 16:13:17 +0200

    Bugfix: Display indicator only if farmer isn't visible.

--- Assets/Adventure/2_Code/UI/FarmerNavigator.cs
+++ Assets/Adventure/2_Code/UI/FarmerNavigator.cs
@@ -50,7 +50,7 @@ namespace Adventure.UI
 		void LateUpdate()
 		{
 			// Calculation only if farmer is visible
-			if(!_renderer.isVisible)
+			if(!IsVisibleFrom(_renderer, CameraControl.active))
 			{
 				// Vector from farmer to currently active camera.
 				_distanceVector = _farmer.position - CameraControl.active.transform.position;
@@ -131,11 +131,10 @@ namespace Adventure.UI
 		/// </summary>
 		protected virtual void OnGUI()
 		{
-			//Drawing.DrawLine(_screenCenter, _indicatorPoint, Color.red);
-			
-			//GUIUtility.RotateAroundPivot(_indicatorAngle * Mathf.Rad2Deg - 90f, _screenCenter);
-			GUI.DrawTexture(new Rect(_indicatorPoint.x - (_indicator.width / 2f) , _indicatorPoint.y - (_indicator.height / 2f), _indicator.width, _indicator.height), _indicator, ScaleMode.ScaleToFit);
-			//GUI.matrix = _matrixBackup;
+			if(!IsVisibleFrom(_renderer, CameraControl.active))
+			{
+				GUI.DrawTexture(new Rect(_indicatorPoint.x - (_indicator.width / 2f), _indicatorPoint.y - (_indicator.height / 2f), _indicator.width, _indicator.height), _indicator, ScaleMode.ScaleToFit);
+			}
 		}
 		
 		/// <summary>
@@ -153,6 +152,30 @@ namespace Adventure.UI
 
 			// p1 * x^3 + p2 * x^2 + p3 * x
 			return p1 * Mathf.Pow(t, 3f) + p2 * Mathf.Pow(t, 2f) + p3 * t;
+		}
+
+		/// <summary>
+		/// Determines if renderer is visible from the specified camera.
+		/// </summary>
+		/// <returns><c>true</c> if is visible from the specified camera; otherwise, <c>false</c>.</returns>
+		/// <param name="renderer">Renderer.</param>
+		/// <param name="camera">Camera.</param>
+		protected bool IsVisibleFrom(Renderer renderer, Camera camera)
+		{
+			Vector3 temp = camera.WorldToViewportPoint(renderer.transform.position);
+			Vector3 rayDir = (renderer.transform.position + Vector3.up * 0.5f) - camera.transform.position;
+
+			if((temp.x >= 0) && (temp.x <= 1) &&
+				(temp.y >= 0) && (temp.y <= 1) &&
+				(temp.z >= 0))
+			{
+				if(!Physics.Raycast(camera.transform.position, rayDir, rayDir.magnitude  - 3f))
+				{
+					return true;
+				}
+			}
+
+			return false;
 		}
 	}
 }
