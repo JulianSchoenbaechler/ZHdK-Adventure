8d64f2149632c2ec58efb7a4aa22fcde2c1998b3
Julian Schönbächler <info@julian-s.ch>
2017-05-30 23:49:37 +0200

    Added speach bubbles. Farmer pitchfork.

---
+++ Assets/Adventure/2_Code/Entity/FarmerWalking.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Adventure.Entity
+{
+	public class FarmerWalking : MonoBehaviour
+	{
+		[SerializeField] protected float _animationSpeed = 2f;
+		[SerializeField] protected float _speed = 1f;
+		[SerializeField] protected float _destroyTime = 5f;
+
+		protected Vector3 _initialPosition;
+
+		protected virtual void Start()
+		{
+			_initialPosition = transform.position;
+			GetComponent<Animator>().speed = _animationSpeed;
+			Invoke("Reset", _destroyTime);
+		}
+
+
+		protected virtual void Update()
+		{
+			transform.position += transform.forward * Time.deltaTime * _speed;
+		}
+
+
+		protected virtual void Reset()
+		{
+			transform.position = _initialPosition;
+			gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Assets/Adventure/2_Code/Entity/FarmerWalkingTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Adventure.Entity
+{
+	[RequireComponent(typeof(Collider))]
+	public class FarmerWalkingTrigger : MonoBehaviour
+	{
+		[SerializeField] protected GameObject _farmer;
+
+		//private bool _doorOpen = false;
+
+		protected virtual void OnTriggerEnter(Collider collider)
+		{
+			if(collider.CompareTag("Player"))
+			{
+				//if(_doorOpen)
+					_farmer.SetActive(true);
+			}/*
+			else if(collider.CompareTag("Barrel"))
+			{
+				_doorOpen = true;
+			}
+			*/
+		}
+	}
+}
